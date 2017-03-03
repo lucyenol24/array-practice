a367328ce9f8ce46f8e51fbf806b64d04e6d19ff
lucyenol24 <lucynolan24@gmail.com>
2017-03-02 20:31:39 -0800

    js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-alert("Let's get to work! ")
+
 // TODO: Build an array of strings that could be answers to magic 8 ball type questions.
 
+var fortunes = ["perhaps", "yaaaas", "nah", "look a squirrel!"]
 
 /* TODO: Write a form handler function that will:
   1. capture the question asked in the form
@@ -8,4 +9,52 @@ alert("Let's get to work! ")
   3. console log something conversational to user, like "Oh, you want to know [question]? Well, I think... [answer]"
 */
 
-var a = ["perhaps", "yaaaas", "nah", "pineapple", "look a squirrel!"]
+function clicker() {
+  console.log("clicked!!!!!");
+}
+
+
+
+function removeAnswer() {
+  
+  var remv = document.removalForm.remove.value;
+  
+  var index = fortunes.indexOf(remv);
+  
+  if(index >= 0) {
+    fortunes.splice(index, 1) 
+//    index and how many elements we want it to take. we only want to remove one element in this example. now fortunes will be one element shorter than it used to be.
+  }
+  
+  console.log(fortunes);
+  
+  event.preventDefault();
+}
+
+
+function insertAnswer() {
+  
+  var answer = document.answerForm.answer.value;
+  
+  fortunes.push(answer);
+  
+  console.log(fortunes);
+  
+  event.preventDefault();
+}
+//HW: pushing an element into a cart array will look like this code. 
+
+function magicAnswer() {
+  
+  var question = document.questionForm.question.value;
+  
+  var rando = Math.floor(Math.random() * fortunes.length);
+  
+  var answer = fortunes[rando];
+  
+  var output = "So you want to know " + question + " ...well I think... " + answer;
+
+  console.log(output);
+  
+  event.preventDefault();
+}
